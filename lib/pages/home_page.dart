5b16227d1b972c0832164056cc50ab5fe68e0533
Kamal <kamal@kahunas.io>
2025-03-02 14:53:11 +0400

    Add Todo list and checkbox functionality

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tasky/util/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +9,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  // list of todos
+  List todos = [
+    ["Learn Flutter Deeply", false],
+    ["Learn Laravel Deeply", false],
+    ["Eat Healthy", true],
+  ];
+
+  void checkboxChanged(bool value, int index) {
+    setState(() {
+      todos[index][1] = !todos[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber,
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('Tasky'),
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (BuildContext context, int index) { 
+          return ToDoTile(taskName: todos[index][0], taskCompleted: todos[index][1], onChanged: (value) => checkboxChanged(value!, index));
+       },
+        
+      ),
     );
   }
 }
---
+++ lib/util/todo_tile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ToDoTile extends StatelessWidget {
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  ToDoTile({super.key, required this.taskName, required this.taskCompleted, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(12)
+        ),
+        child: Row(
+          children: [
+            // Checkbox
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+            // Task Name
+            Text(taskName),
+          ],
+        ),
+      ),
+    );
+  }
+}
